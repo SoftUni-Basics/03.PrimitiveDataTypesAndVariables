20f1814030c7bc0aa2fc5b163310a9d65e06dfc6
GogoK <georgiakanchev@gmail.com>
2015-09-12 21:42:51 +0300

    adding 03. Primitive data types solution

---
+++ 03.VariableInHexadecimalFormat/HexadecimalFormats.cs
@@ -0,0 +1,14 @@
+﻿/*
+    Declare an integer variable and assign it with the value 254 in hexadecimal format (0x##). Use Windows Calculator to find its hexadecimal representation. Print the variable and ensure that the result is “254”.
+ */
+
+using System;
+
+class HexadecimalFormats
+{
+    static void Main()
+    {
+        int hexa = 0xFE;
+        Console.WriteLine("\"0x{0:X}\" == {0}", hexa);
+    }
+}
