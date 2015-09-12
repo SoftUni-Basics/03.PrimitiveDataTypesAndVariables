42f6ec97e2b9238a0fa5727900f49099b31ed801
GogoK <georgiakanchev@gmail.com>
2015-09-12 23:22:45 +0300

    adding 07. quotes in string solution.

---
+++ 07.QuotesInStrings/StringQuotes.cs
@@ -0,0 +1,16 @@
+﻿/*  Declare two string variables and assign them with following value:
+        The "use" of quotations causes difficulties.
+ */
+
+using System;
+
+class StringQuotes
+{
+    static void Main()
+    {
+        string first = @"The ""use"" of quotations causes difficulties.";
+        string second = "The \"use\" of quotations causes difficulties.";
+
+        Console.WriteLine("{0}\r\n{1}", first, second);
+    }
+}
