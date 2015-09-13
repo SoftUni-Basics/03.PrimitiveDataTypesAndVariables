a52469e047d74b66d3eca9b2a9dacfb66140dc69
GogoK <georgiakanchev@gmail.com>
2015-09-13 03:33:24 +0300

    solution for 09 exchange values

---
+++ 09.ExchangeVariableValues/ExchangeValues.cs
@@ -0,0 +1,18 @@
+﻿/*  Declare two integer variables a and b and assign them with 5 and 10 and after that exchange their values by using some programming logic. Print the variable values before and after the exchange.
+    Expected Output:  Before: a = 5, b = 10; After: a = 10, b = 5;
+ */
+using System;
+
+class ExchangeValues
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+        Console.WriteLine("Before:\r\na = {0}\r\nb = {1}", a, b);
+        
+        b = a;
+        a += b;
+        Console.WriteLine("After:\r\na = {0}\r\nb = {1}", a, b);
+    }
+}
