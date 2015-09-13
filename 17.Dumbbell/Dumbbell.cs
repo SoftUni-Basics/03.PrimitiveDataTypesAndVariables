4793ef2b56ff1255eaebef5e7c990f31c659fe6f
GogoK <georgiakanchev@gmail.com>
2015-09-13 23:39:07 +0300

    add exam 3

---
+++ 17.Dumbbell/Dumbbell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Dumbbell
+{
+    static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("{0}{1}&{2}&{1}{0}", new string('.', input / 2), new string('&', input / 2), new string('.', input));
+        for (int i = 1; i <= (input / 2) - 1; i++)
+        {
+            Console.WriteLine("{0}&{1}&{2}&{1}&{0}",
+                new string('.', (input / 2) - i), new string('*', (input / 2) + i-1), new string('.', input));
+        }
+        Console.WriteLine("&{0}&{1}&{0}&", new string('*', input - 2), new string('=', input));
+        for (int i = 1; i <= (input / 2) - 1; i++)
+        {
+            Console.WriteLine("{0}&{1}&{2}&{1}&{0}", 
+                new string('.', i), new string('*', (input - 2) - i), new string('.', input));
+        }
+        Console.WriteLine("{0}{1}&{2}&{1}{0}", new string('.', input / 2), new string('&', input / 2), new string('.', input));
+    }
+}
