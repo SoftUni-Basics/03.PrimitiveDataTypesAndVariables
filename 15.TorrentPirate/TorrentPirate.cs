23a109ccd5137b3449e4d86bc4905981ade18abf
GogoK <georgiakanchev@gmail.com>
2015-09-13 22:16:59 +0300

    exam1

---
+++ 15.TorrentPirate/TorrentPirate.cs
@@ -0,0 +1,28 @@
+﻿/*  Exam Problems 1 */
+
+using System;
+
+class TorrwntPirate
+{
+    static void Main()
+    {
+        double download = int.Parse(Console.ReadLine());
+        int cinemaPrice = int.Parse(Console.ReadLine());
+        int wifeSpend = int.Parse(Console.ReadLine());
+
+        double downloadTime = download / (2 * 60 * 60);
+        double downloadMovies = download / 1500;
+        double wifeCost = downloadTime * wifeSpend;
+
+        double cinema = cinemaPrice * downloadMovies;
+
+        if (cinema < wifeCost)
+        {
+            Console.WriteLine("cinema -> {0:F2}lv", cinema);
+        }
+        else if (wifeCost <= cinema)
+        {
+            Console.WriteLine("mall -> {0:F2}lv", wifeCost);
+        }
+    }
+}
